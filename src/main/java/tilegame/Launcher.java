9479c7db7ea1d7f7fbaff52a4414ec53827f05a9
Alec Meyer <11meyal@gmail.com>
2021-12-01 21:16:15 -0500

    Added JFrame

--- src/main/java/tilegame/Launcher.java
+++ src/main/java/tilegame/Launcher.java
@@ -1,9 +1,11 @@
 package tilegame;
 
+import tilegame.display.Display;
+
 public class Launcher {
 
     public static void main(String[] args) {
-
+        new Display("Title!", 300, 300);
     }
 
 }
--- src/main/java/tilegame/display/Display.java
+++ src/main/java/tilegame/display/Display.java
@@ -10,4 +10,23 @@ public class Display {
     private int width;
     private int height;
 
+    public Display(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+
+        createDisplay();
+    }
+
+    private void createDisplay() {
+        frame = new JFrame(title);
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        // Set window to center of screen
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
 }
