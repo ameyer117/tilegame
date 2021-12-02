74771ad8612ab953424ed014cd108a5274a39ce5
Alec Meyer <11meyal@gmail.com>
2021-12-01 21:03:35 -0500

    TileGame

---
+++ src/main/java/tilegame/Launcher.java
@@ -0,0 +1,9 @@
+package tilegame;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/tilegame/display/Display.java
@@ -0,0 +1,13 @@
+package tilegame.display;
+
+import javax.swing.*;
+
+public class Display {
+
+    private JFrame frame;
+
+    private String title;
+    private int width;
+    private int height;
+
+}
